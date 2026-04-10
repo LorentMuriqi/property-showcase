9c8e2746dd7ceeee333d1e19e2b19f53b2726928
Lorent <lorentmuriqi@gmail.com>
2026-04-10 23:48:16 +0200

    remove hyrja e agjentit - footer

--- artifacts/real-estate/src/components/Layout.tsx
+++ artifacts/real-estate/src/components/Layout.tsx
@@ -175,8 +175,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </div>
 
             <div>
-              {/* Main Content <h4 className="font-display text-lg text-white mb-6">Lidhje të Shpejta</h4>  */}
-			  <h3 </h3>
+              <h4 className="font-display text-lg text-white mb-6"> </h4>
+			  
               <ul className="space-y-3">
                 {navLinks.filter(l => !['Paneli Administrativ'].includes(l.name)).map(link => (
                   <li key={link.href}>
